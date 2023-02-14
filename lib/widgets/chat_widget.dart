693c953ad085df071bea8f7380500e9295f6c006
Victor Mauboussin <victor.mauboussin@outlook.fr>
2023-02-14 16:46:30 +0100

    Adding Chat_Widget.dart, Chat_screen.dart and modifying constants.dart

--- lib/widgets/chat_widget.dart
+++ lib/widgets/chat_widget.dart
@@ -1,28 +1,39 @@
 import 'package:chatgpt_orbis/constants/constants.dart';
 import 'package:chatgpt_orbis/services/assets_manager.dart';
+import 'package:chatgpt_orbis/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'text_widget.dart';
 
 class ChatWidget extends StatelessWidget {
-  const ChatWidget({super.key});
+  const ChatWidget({super.key, required this.msg, required this.chatIndex});
 
+  final String msg;
+  final int chatIndex;
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Material(
-          color: cardColor,
+          color: chatIndex == 0 ? scaffoldBackgroundColor : cardColor,
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Row(
               children: [
                 Image.asset(
-                  AssetsManager.userImage,
+                  chatIndex == 0
+                      ? AssetsManager.userImage
+                      : AssetsManager.botImage,
                   height: 30,
                   width: 30,
                 ),
-                const Text("Here will be the message"),
+                const SizedBox(
+                  width: 8,
+                ),
+                TextWidget(
+                  label: msg,
+                ),
               ],
             ),
           ),
