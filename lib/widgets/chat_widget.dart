12dafa5faddd0315c543e543b9a97a016d951222
Victor Mauboussin <victor.mauboussin@outlook.fr>
2023-02-16 21:47:37 +0100

    Changed chatMessages informations in constants and chat_widget

--- lib/widgets/chat_widget.dart
+++ lib/widgets/chat_widget.dart
@@ -2,9 +2,6 @@ import 'package:chatgpt_orbis/constants/constants.dart';
 import 'package:chatgpt_orbis/services/assets_manager.dart';
 import 'package:chatgpt_orbis/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-import 'text_widget.dart';
 
 class ChatWidget extends StatelessWidget {
   const ChatWidget({super.key, required this.msg, required this.chatIndex});
