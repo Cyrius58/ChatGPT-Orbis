3443840fbf1d77ad18a1f2e2f7954c4af131ced3
Victor Mauboussin <victor.mauboussin@outlook.fr>
2023-02-17 00:42:17 +0100

    revert last commit (commentaries)

--- lib/providers/models_provider.dart
+++ lib/providers/models_provider.dart
@@ -13,7 +13,6 @@ class ModelsProvider with ChangeNotifier {
     notifyListeners();
   }
 
-/*
   List<ModelsModel> modelsList = [];
   List<ModelsModel> get getModelsList {
     return modelsList;
@@ -22,5 +21,5 @@ class ModelsProvider with ChangeNotifier {
   Future<List<ModelsModel>> getAllModels() async {
     modelsList = await ApiService.getModels();
     return modelsList;
-  }*/
+  }
 }
